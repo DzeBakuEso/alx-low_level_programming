69dc971c50488b02574adae8364a5eba1c8218c1
DzeBakuEso <dzeblebaku@gmail.com>
2024-06-27 21:27:39 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,13 @@
  */
 int _isupper(int c);
 
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c);
+
 #endif /* MAIN_H */
 
