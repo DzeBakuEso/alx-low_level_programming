50af770f9cdb64f7b953ba5da07e66b991925bd5
DzeBakuEso <dzeblebaku@gmail.com>
2024-07-08 14:58:30 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
 
