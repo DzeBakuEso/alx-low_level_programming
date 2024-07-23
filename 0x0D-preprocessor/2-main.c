67fe87885f9f3906472cdc8839c09be0abd44ecb
DzeBakuEso <dzeblebaku@gmail.com>
2024-07-22 19:02:07 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Print the name of the file it was compiled from
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%s\n", __FILE__);  /**< Print the name of the file */
+    return (0);
+}
+
