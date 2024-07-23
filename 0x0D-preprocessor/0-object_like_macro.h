c4c2381283404679629d74906e40e37f327941ac
DzeBakuEso <dzeblebaku@gmail.com>
2024-07-22 18:50:31 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * SIZE - Macro to define a size constant
+ *
+ * Value: 1024
+ */
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
+
