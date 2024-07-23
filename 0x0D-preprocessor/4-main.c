b114463d01b4684fb8a33bec00b11055daebb6ba
DzeBakuEso <dzeblebaku@gmail.com>
2024-07-22 19:05:36 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "4-sum.h"
+
+/**
+ * main - Check the code for the macro SUM
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);  /**< Calculate the sum of 98 and 1024 */
+    printf("%d\n", s);  /**< Print the result */
+    return (0);
+}
+
