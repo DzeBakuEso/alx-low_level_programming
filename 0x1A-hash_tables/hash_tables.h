5433bb1efef057d4157e48d064b87b0676bde1e1
DzeBakuEso <dzeblebaku@gmail.com>
2024-10-11 17:30:10 -0700

    2-key_index.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,4 +34,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 
 /* Header declarations */
 unsigned long int hash_djb2(const unsigned char *str);
+
+/*implement the key_index function for your hash table*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif /* HASH_TABLES_H */
